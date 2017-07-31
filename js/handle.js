a4f0053a0d2dd0041d8d2616ddb8e1c689d50010
Ali <ali.kholmatov@gmail.com>
2017-07-31 12:58:22 +0300

    I am Slider,Hello

--- js/handle.js
+++ js/handle.js
@@ -9,18 +9,54 @@ var swiperV = new Swiper('.swiper-container-v', {
     direction: 'vertical',
     longSwipes: false,
     followFinger: false,
-   // height:document.documentElement.clientHeight
 
 });
 
 //onSlideChangeEnd(swiper)
-    swiperV.on('slideChangeStart', function () {
+swiperV.on('slideChangeStart', function () {
 
-    });
+});
+
+var swiperH = new Swiper('.swiper-container-h', {
+    pagination: null,
+    direction: 'horizontal'
+});
 
 
-    var swiperH=new Swiper('.swiper-container-h',{
-        pagination:null,
-        direction: 'horizontal'
-    });
+var slider = document.querySelector('.range-slider');
+noUiSlider.create(slider, {
+    start: 0,
+    range: {
+        'min': 0,
+        'max': 2
+    }
+});
 
+slider.noUiSlider.on('change', function () {
+    autoSet(this);
+
+});
+
+slider.noUiSlider.on('slide', function () {
+    handleSet(this);
+
+});
+
+function autoSet(thumb) {
+    var thumbPosition = Math.round(thumb.get());
+    thumb.set(thumbPosition);
+    swiperH.slideTo(thumbPosition);
+};
+function handleSet(thumb) {
+    var thumbPosition = thumb.get();
+    if (thumbPosition < 0.5) {
+        swiperH.slideTo(0);
+    }
+    else if (thumbPosition >= 0.5 && thumbPosition < 1.5) {
+        swiperH.slideTo(1);
+    }
+    else {
+        swiperH.slideTo(2);
+    }
+
+}
